b8948e99fd883a5e302b252730457275009657c9
SHARAN <SHARAN@192.168.1.78>
2017-10-02 13:50:33 +1300

    Updated with modifications

---
+++ JenkinsDemo/src/test/java/RegisterTest.java
@@ -0,0 +1,33 @@
+import static org.junit.Assert.*;
+
+import org.junit.Before;
+import org.junit.Test;
+
+public class RegisterTest {
+	Register obj;
+	int x,y,z;
+	@Before
+	public void setUp() throws Exception {
+		x=5;
+		y=6;
+		z=4;
+		obj=new Register();
+	}
+	
+
+	@Test
+	public void testaverage() {
+		assertEquals(5,obj.average(x,y,z));
+			}
+	
+	@Test
+		public void testaverage1() {
+		assertEquals(8,obj.average(x,y,z));
+			}
+	
+	@Test
+		public void testaverage2() {
+			assertEquals(5,obj.average(x,y,z));
+				}
+	
+}
