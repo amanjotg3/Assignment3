08f96598a527cf7f2931c1f0b022be2cc51a2e30
SHARAN <SHARAN@192.168.1.78>
2017-10-02 13:55:43 +1300

    Configured to Jenkins

--- JenkinsDemo/src/test/java/RegisterTest.java
+++ JenkinsDemo/src/test/java/RegisterTest.java
@@ -3,7 +3,8 @@ import static org.junit.Assert.*;
 import org.junit.Before;
 import org.junit.Test;
 
-public class RegisterTest {
+public class RegisterTest 
+{
 	Register obj;
 	int x,y,z;
 	@Before
